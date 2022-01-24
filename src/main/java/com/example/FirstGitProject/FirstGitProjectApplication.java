062ca2c53882cebdecafe1b6904a6728621b9347
pradhanmol <pradhanmol30@gmail.com>
2022-01-24 13:06:11 +0530

    reverse string program

--- src/main/java/com/example/FirstGitProject/FirstGitProjectApplication.java
+++ src/main/java/com/example/FirstGitProject/FirstGitProjectApplication.java
@@ -1,13 +1,31 @@
 package com.example.FirstGitProject;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Scanner;
+
+
 @SpringBootApplication
-public class FirstGitProjectApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FirstGitProjectApplication.class, args);
-	}
+		public class FirstGitProjectApplication {
+	
 
+		public static void main(String[] args) {
+			Scanner in = new Scanner(System.in);
+			System.out.println("Enter string");
+			String str = in.nextLine();
+			System.out.println(str);
+			StrRev(str);
+			
+		}
+		public static void StrRev(String Str) {
+			char cha[] = Str.toCharArray();
+			String reverse = "";
+			for (int i = cha.length - 1; i >= 0; i--) {
+				reverse += cha[i];
+			}
+			System.out.println(reverse);
+		}
+	
 }
+
