2e061d944d3cdef6c79bc438d934da0fe76f22a7
pradhanmol <pradhanmol30@gmail.com>
2022-01-24 13:10:05 +0530

    Reverse String

--- src/main/java/com/example/FirstGitProject/FirstGitProjectApplication.java
+++ src/main/java/com/example/FirstGitProject/FirstGitProjectApplication.java
@@ -6,26 +6,26 @@ import java.util.Scanner;
 
 
 @SpringBootApplication
-
-		public class FirstGitProjectApplication {
 	
+			public class FirstGitProjectApplication {
 
-		public static void main(String[] args) {
-			Scanner in = new Scanner(System.in);
-			System.out.println("Enter string");
-			String str = in.nextLine();
-			System.out.println(str);
-			StrRev(str);
-			
-		}
-		public static void StrRev(String Str) {
-			char cha[] = Str.toCharArray();
-			String reverse = "";
-			for (int i = cha.length - 1; i >= 0; i--) {
-				reverse += cha[i];
+
+			public static void main(String[] args) {
+				Scanner in = new Scanner(System.in);
+				System.out.println("Enter string");
+				String str = in.nextLine();
+				System.out.println(str);
+				StrRev(str);
+
 			}
-			System.out.println(reverse);
-		}
-	
-}
+			public static void StrRev(String Str) {
+				char cha[] = Str.toCharArray();
+				String reverse = "";
+				for (int i = cha.length - 1; i >= 0; i--) {
+					reverse += cha[i];
+				}
+				System.out.println(reverse);
+			}
+
+	}
 
